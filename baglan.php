b4a5faf99439b39e1606b228801ac6b9bd104f9a
raymastaryo0123 <77002824+raymastaryo0123@users.noreply.github.com>
2021-01-05 17:18:15 +0300

    Update baglan.php

--- baglan.php
+++ baglan.php
@@ -2,7 +2,8 @@
 
 try {
 
-	$db=new PDO("mysql:host=localhost;dbname=odev;charset=utf8",'root');
+	//$db=new PDO("mysql:host=localhost;dbname=odev;charset=utf8",'root');
+    $db=new PDO("mysql:host=remotemysql.com;dbname=uplLC4YwYx;charset=utf8",'uplLC4YwYx','5dJPF6VsTH');
 
 } catch (PDOExpception $e) {
 
